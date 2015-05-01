5aa3100ede5c083e4869ff3d47031ec87c32aa95
mmoontoro <montoromendoza.mariano@gmail.com>
2015-05-01 01:45:42 -0700

    Lista de preguntas 1

--- models/models.js
+++ models/models.js
@@ -23,6 +23,8 @@ sequelize.sync().then(function() {
 	Quiz.count().then(function (count) {
 		if( count == 0 ) {
 			Quiz.create({pregunta: 'Capital de Italia',
+					respuesta: 'Roma'});
+			Quiz.create({pregunta: 'Capital de Portugal',
 					respuesta: 'Roma'}).then(function() { console.log('Base de datos inicializada')});
 		};
 	});
